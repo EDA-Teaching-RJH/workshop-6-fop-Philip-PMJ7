c2121f3a389ccacf4bd1ad3e366ebfd853c14b92
Philip-PMJ7 <pmj7@kent.ac.uk>
2026-02-09 12:10:48 +0000

    Completed Part 2

--- part_2.py
+++ part_2.py
@@ -1,1 +1,19 @@
+rover_status = {
+    "Battery":100, "Heater":"Off", "Camera":"Standby"
+    }
 
+print(rover_status["Battery"])
+
+
+rover_status["Battery"] = 85
+rover_status["Heater"] = "On"  # Change marks
+rover_status.update({"Speed": 5})
+print(rover_status)
+
+mission_log = [
+    {"Site": "Crater A", "Radiation": "Low", "Water": False},
+    {"Site": "Dune B", "Radiation": "High", "Water": True}
+    ]
+
+for mission in mission_log:
+    print("Site ", mission["Site"], " has ", mission["Radiation"], " radiation.")
