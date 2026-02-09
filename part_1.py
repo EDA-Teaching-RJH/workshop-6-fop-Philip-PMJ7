e6b11e3dc3fa6b2adcf4cfedd439acc5bb5153c4
Philip-PMJ7 <pmj7@kent.ac.uk>
2026-02-09 11:37:37 +0000

    Part 1 Complete

--- part_1.py
+++ part_1.py
@@ -1,1 +1,21 @@
+sample_bay = ["Basalt", "Silica", "Iron", "Dust"]
 
+print(sample_bay[0])
+print(sample_bay[len(sample_bay)-1])
+print(len(sample_bay))
+
+for item in range(len(sample_bay)):
+    print("Transmitting data for: ", sample_bay[item])
+
+new_findings = []
+i = 0
+for x in range(0,3):
+    new_rock = input("New Rock Detcted - Enter name: ")
+    new_findings.append(new_rock)
+    print(new_findings)
+
+if "Dust" in sample_bay:
+    sample_bay.remove("Dust")
+    print(sample_bay)
+else:
+    print("No dust detected.")
